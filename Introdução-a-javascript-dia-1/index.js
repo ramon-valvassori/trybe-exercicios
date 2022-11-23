548bda19450a8bc962338c0cadefea7b2e408606
Ramon <ramonepico@gmail.com>
2022-11-23 15:59:34 -0300

    Operações aritméticas

--- Introdução-a-javascript-dia-1/index.js
+++ Introdução-a-javascript-dia-1/index.js
@@ -1,15 +1,7 @@
-const myName = "Ramon";
-const birthCity = "Criciúma";
-let birthYear = 1987
+const base = 5;
+const height = 8;
+const area = 40;
+console.log(base*height)
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthyear);
-
-byrthYear = 2030;
-
-console.log(byrthYear);
-
-birthCity = "Curitiba";
-
-console.log(bithCity);
+const perimeter = 53
+console.log(base+height+area)
