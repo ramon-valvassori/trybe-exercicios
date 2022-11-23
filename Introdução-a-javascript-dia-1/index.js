242b33982217695527ca80e7a7350397ca015932
Ramon <ramonepico@gmail.com>
2022-11-23 15:46:29 -0300

    Exercício de variáveis

---
+++ Introdução-a-javascript-dia-1/index.js
@@ -0,0 +1,15 @@
+const myName = "Ramon";
+const birthCity = "Criciúma";
+let birthYear = 1987
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthyear);
+
+byrthYear = 2030;
+
+console.log(byrthYear);
+
+birthCity = "Curitiba";
+
+console.log(bithCity);
